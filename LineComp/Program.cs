c5d05abdb0e798c29d603ccb9d948998fdc41e1e
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-19 11:21:37 +0530

    [jayesh] Equal length of line

--- LineComp/Program.cs
+++ LineComp/Program.cs
@@ -27,6 +27,41 @@
 
 			Console.WriteLine("The length of line is: " + length);
 
+			//Taking Co-ordinates as a variables for Line 2
+			int x3, y3, x4, y4;
+
+			//Taking Inputs from users for line 2
+			Console.WriteLine("**Computation for line 2**");
+
+			Console.WriteLine("Enter the value of x3: ");
+			x3 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter the value of x4: ");
+			x4 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter the value of y3: ");
+			y3 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter the value of y4: ");
+			y4 = Convert.ToInt32(Console.ReadLine());
+
+			//Defining the Length of line
+			double length2 = Math.Sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+
+			Console.WriteLine("The length of line2 is: " + length2);
+
+			//comparing the lengths of lines 1 & 2
+			if (length == length2)
+			{
+				Console.WriteLine("The given lines are equals: ");
+			}
+			else
+			{
+				Console.WriteLine("The given lines are not equal: ");
+
+			}
+
+
 		}
 		public static void Main(string[] args)
 		{
